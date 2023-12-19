28e0af789fd5672b6d558939fa5b45663deedda5
Regis Costa Pedro <regiscostapedro0705@gmail.com>
2023-12-18 21:15:45 -0300

    adicao do download do Visual Studio Code na pagina de dowloads

--- views/downloads.php
+++ views/downloads.php
@@ -1,125 +1,150 @@
-
-
 <br>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-<link href="https://fonts.cdnfonts.com/css/diavlo-black" rel="stylesheet">
-                
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/diavlo-black" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
     <style>
-    body {
-        color: #000;
-        font-family: 'Montserrat', sans-serif;
-        background-image: url(img/bgDownload.png) ;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-blend-mode: soft-light;
-    }
-    h1{
-        text-align: center;
-        font-family: 'Diavlo Black', sans-serif;
-        font-family: 'Diavlo Book', sans-serif;
-         font-family: 'Diavlo Light', sans-serif;
-          font-family: 'Diavlo Medium', sans-serif;
-           font-family: 'Diavlo Bold', sans-serif;
-    }
+        body {
+            color: #000;
+            font-family: 'Montserrat', sans-serif;
+            background-image: url(img/bgDownload.png);
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-blend-mode: soft-light;
+        }
 
-    img {
-        width: 200px !important;
-        border-radius: 10px;
-        margin: auto;
+        h1 {
+            text-align: center;
+            font-family: 'Diavlo Black', sans-serif;
+            font-family: 'Diavlo Book', sans-serif;
+            font-family: 'Diavlo Light', sans-serif;
+            font-family: 'Diavlo Medium', sans-serif;
+            font-family: 'Diavlo Bold', sans-serif;
+        }
 
-    }
+        img {
+            width: 200px !important;
+            border-radius: 10px;
+            margin: auto;
 
-    .container .col-sm {
-        border: 1px solid red;
-    }
-    .card{
-        border: 4px solid #5fb0b7;
-  border-bottom: 4px solid #79f3c9;
-  border-right: 4px solid #79f3c9;
-  color: #000;
-    }
+        }
 
-    p{
-        font-size: 14px;
-    }
-</style>
+        .container .col-sm {
+          
+            border: 1px solid red;
+        
+        }
+
+        .card {
+        
+            border: 4px solid #5fb0b7;
+            border-bottom: 4px solid #79f3c9;
+            border-right: 4px solid #79f3c9;
+            color: #000;
+            margin-top: 30px;
+        }
+
+        #card{
+            gap: 30px;
+        }
+
+        p {
+            font-size: 14px;
+        }
+    </style>
 </head>
+
 <body>
-<h1 align=center style="color:white">Download de arquivos para estudo</h1>
+    <h1 align=center style="color:white">Download de arquivos para estudo</h1>
 
 
-<!-- Carousel wrapper -->
-<div id="carouselMultiItemExample" class="carousel slide carousel-dark text-center" data-mdb-ride="carousel">
-    <!-- Controls -->
-    <div class="d-flex justify-content-center mb-4">
+    <!-- Carousel wrapper -->
+    <div id="carouselMultiItemExample" class="carousel slide carousel-dark text-center" data-mdb-ride="carousel">
+        <!-- Controls -->
+        <div class="d-flex justify-content-center mb-4">
 
 
-    </div>
-    <!-- Inner -->
-    <div class="carousel-inner py-4">
-        <!-- Single item -->
-        <div class="carousel-item active">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card" >
-                            <img src="img/htmlcssjs-overview.png" class="card-img-top" alt="" />
-                            <div class="card-body">
-                                <h5 class="card-title">Curso de HTML,CSS e JavaScript</h5>
-                                <p class="card-text">    
-                                Explore o Curso de HTML, CSS e JavaScript para dominar o desenvolvimento web. Estrutura com HTML, estilize com CSS, adicione dinamismo com JavaScript.
+        </div>
+        <!-- Inner -->
+        <div class="carousel-inner py-4">
+            <!-- Single item -->
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <div class="card">
+                                <img src="img/htmlcssjs-overview.png" class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Curso de HTML,CSS e JavaScript</h5>
+                                    <p class="card-text">
+                                        Explore o Curso de HTML, CSS e JavaScript para dominar o desenvolvimento web. Estrutura com HTML, estilize com CSS, adicione dinamismo com JavaScript.
 
-                                </p>
-                                <a href="https://www.caelum.com.br/apostila/apostila-html-css-javascript.pdf" download="Apostila-Js-html-css.pdf" target="_blank" class="btn btn-primary">Download</a>
+                                    </p>
+                                    <a href="https://www.caelum.com.br/apostila/apostila-html-css-javascript.pdf" download="Apostila-Js-html-css.pdf" target="_blank" class="btn btn-primary">Download</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-lg-4 d-none d-lg-block">
-                        <div class="card" >
-                            <img src="img/php.jpg" class="card-img-top" alt="" />
-                            <div class="card-body">
-                                <h5 class="card-title"> Apostila de PHP</h5>
-                                <p class="card-text">
+                        <div class="col-lg-4 d-none d-lg-block" id="">
+                            <div class="card">
+                                <img src="img/php.jpg" class="card-img-top" alt="" />
+                                <div class="card-body">
+                                    <h5 class="card-title"> Apostila de PHP</h5>
+                                    <p class="card-text">
 
-                                    Bem-vindo ao curso de PHP, onde você vai dominar os fundamentos e avanços da linguagem para criar aplicações web dinâmicas. . Vamos iniciar essa jornada rumo ao domínio do PHP!
-                                </p>
-                                <a href="http://www.etelg.com.br/paginaete/downloads/informatica/apostila_php.pdf" download="Apostila_PHP" target="_blank" class="btn btn-primary">Download</a>
+                                        Bem-vindo ao curso de PHP, onde você vai dominar os fundamentos e avanços da linguagem para criar aplicações web dinâmicas. . Vamos iniciar essa jornada rumo ao domínio do PHP!
+                                    </p>
+                                    <a href="http://www.etelg.com.br/paginaete/downloads/informatica/apostila_php.pdf" download="Apostila_PHP" target="_blank" class="btn btn-primary">Download</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
 
-                    <div class="col-lg-4 d-none d-lg-block">
-                        <div class="card" >
-                            <img src="img/java-logo-1.png" class="card-img-top" alt="Sunset over the Sea" />
-                            <div class="card-body">
-                                <h5 class="card-title">Curso de JAVA e Orientação a objetos</h5>
-                                <p class="card-text">
-                                    Explore JAVA e POO neste curso prático para construção eficiente de aplicações. Bem-vindo a uma jornada envolvente na programação poderosa.
-                                </p>
-                                <a href="https://www.caelum.com.br/apostila/apostila-java-orientacao-objetos.pdf" class="btn btn-primary" target="_blank">Download</a>
+                        <div class="col-lg-4 d-none d-lg-block">
+                            <div class="card">
+                                <img src="img/java-logo-1.png" class="card-img-top" alt="Sunset over the Sea" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Curso de JAVA e Orientação a objetos</h5>
+                                    <p class="card-text">
+                                        Explore JAVA e POO neste curso prático para construção eficiente de aplicações. Bem-vindo a uma jornada envolvente na programação poderosa.
+                                    </p>
+                                    <a href="https://www.caelum.com.br/apostila/apostila-java-orientacao-objetos.pdf" class="btn btn-primary" target="_blank">Download</a>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col-lg-4 d-none d-lg-block">
+                            <div class="card">
+                                <img src="img/vsCode.png" class="card-img-top" alt="Sunset over the Sea" />
+                                <div class="card-body">
+                                    <h5 class="card-title">Visual Studio Code</h5>
+                                    <p class="card-text" >
+
+                                       
+O Visual Studio Code, da Microsoft, é o editor de código mais usado dentre os progamadores, ele possui suporte a várias linguagens e integração eficaz com controle de versão,tornando-se popular com  atualizações frequentes e melhorias contínuas.
+                                    </p>
+                                    <a href="https://go.microsoft.com/fwlink/?Linkid=852157https://go.microsoft.com/fwlink/?Linkid=852157" class="btn btn-primary" target="_blank">Download</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-    </body>
 </body>
+</body>
+
 </html>
-
-
 
